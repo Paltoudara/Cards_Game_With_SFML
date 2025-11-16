836b1b0f1d457ab0aae01de1aeca43e7ed4b1608
panagiotis dimitropoulos <162046043+Paltoudara@users.noreply.github.com>
2025-11-16 19:21:43 +0200

    Refactor texture loading and add shuffle functionality

--- header.h
+++ header.h
@@ -5,6 +5,8 @@
 #include<string>
 #include<unordered_map>
 #include<cstdlib>
+#include<random>
+#include<optional>
 inline void load_file_paths(std::vector<std::string>&file_paths) {
 	//
 	for (std::size_t i = 2; i < 10; i++) {
@@ -45,13 +47,35 @@ inline void load_file_paths(std::vector<std::string>&file_paths) {
 	file_paths.emplace_back("C:\\Users\\user\\source\\repos\\Project_practice\\playing-cards-pack\\PNG\\Cards (large)\\card_spades_Q.png");
 	//
 }
-//
-inline void load_textures_from_files(std::unordered_map<std::size_t, sf::Texture>& deck,std::vector<std::string>&file_paths) {
-	sf::Texture texture{};
-	for (std::size_t i = 1; i <= file_paths.size(); i++) {
-		if (!texture.loadFromFile(file_paths[i - 1])) {
+//create a vector deck
+inline void load_textures_from_files(std::vector<sf::Texture>&textures,
+	std::vector<std::pair<std::size_t, sf::Sprite>>& deck,std::vector<std::string>&file_paths) {
+	textures.resize(file_paths.size());
+
+	for (std::size_t i = 0; i < file_paths.size(); i++) {
+
+		if (!textures[i].loadFromFile(file_paths[i])) {
 			std::exit(1);
 		}
-		deck[i] = texture;
+
+		sf::Sprite sprite{ textures[i] }; // sprite uses stable texture reference
+		deck.emplace_back(i + 1, sprite);
+	}
+}
+//
+inline void shuffle_deck(std::vector<std::pair<std::size_t, sf::Sprite>>& deck) {
+	std::random_device rd{};
+	std::mt19937 gen(rd());
+	std::shuffle(deck.begin(), deck.end(), gen);
+}
+//
+inline void give_players_cards(std::unordered_map<std::size_t, sf::Sprite>&player1
+, std::unordered_map<std::size_t, sf::Sprite>&player2
+, std::vector<std::pair<std::size_t, sf::Sprite>>& deck) {
+	for (std::size_t i = 0; i < 7; i++) {
+		player1.emplace(deck.back().first, deck.back().second);
+		deck.pop_back();
+		player2.emplace(deck.back().first, deck.back().second);
+		deck.pop_back();
 	}
 }
